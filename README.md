ea5e35b6b8d58cc6100a9edb9df35261ffb4090b
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-10-11 10:06:04 +0000

    Reproduction

---
+++ README.md
@@ -0,0 +1,5 @@
+### Reproduction
+
+1. `yarn`
+2. `yarn api-extractor run`
+3. Inspect `dist/repro-initializer.d.ts` (see that import to `SomeEnum` is dropped).
---
+++ src/index.d.ts
@@ -0,0 +1,6 @@
+import {SomeEnum, OtherEnum} from 'some-lib';
+
+export declare class Bla {
+    readonly someField = SomeEnum.Value;
+    readonly workingField: OtherEnum.Value
+}
